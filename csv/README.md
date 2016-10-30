90c251dc8601c34476e8ad3a632e612c339c4600
Brian Brazil <brian.brazil@robustperception.io>
2016-10-30 20:12:25 +0000

    Add example program to output queries in CSV

---
+++ csv/README.md
@@ -0,0 +1,8 @@
+# CSV examples
+
+This directory contains brief example programs that output
+data from Prometheus in a CSV format.
+
+It is presumed you have the `requests` package installed.
+
+For more information see https://www.robustperception.io/prometheus-query-results-as-csv
---
+++ csv/query_csv.py
@@ -0,0 +1,35 @@
+import csv
+import requests
+import sys
+
+"""
+A simple program to print the result of a Prometheus query as CSV.
+"""
+
+if len(sys.argv) != 3:
+    print('Usage: {0} http://prometheus:9090 a_query'.format(sys.argv[0]))
+    sys.exit(1)
+
+response = requests.get('{0}/api/v1/query'.format(sys.argv[1]),
+        params={'query': sys.argv[2]})
+results = response.json()['data']['result']
+
+# Build a list of all labelnames used.
+labelnames = set()
+for result in results:
+      labelnames.update(result['metric'].keys())
+
+# Canonicalize
+labelnames.remove('__name__')
+labelnames = sorted(labelnames)
+
+writer = csv.writer(sys.stdout)
+# Write the header,
+writer.writerow(['name', 'timestamp', 'value'] + labelnames)
+
+# Write the sanples.
+for result in results:
+    l = [result['metric'].get('__name__', '')] + result['value']
+    for label in labelnames:
+        l.append(result['metric'].get(label, ''))
+    writer.writerow(l)
